2cd250de2c9184c48984e935e9a2b09a645100a6
Nazar Kovalenko <kn31132@gmail.com>
2021-12-27 17:46:25 +0200

    Added command /id

--- bot.py
+++ bot.py
@@ -14,7 +14,9 @@ async def ping_command(message: types.Message):
 async def ping_command(message: types.Message):
     await message.reply("Привет!. Чтобы посмотреть что я умею \n/help")
 
-
+@dp.message_handler(commands=['id'])
+async def id_command(message: types.Message):
+    await message.reply("ID пользователя " + str(message.from_user.first_name) + ":\n" + "`" + str(message.from_user.id) + "`", parse_mode='MarkdownV2')
 
 if __name__ == '__main__':
     executor.start_polling(dp)
